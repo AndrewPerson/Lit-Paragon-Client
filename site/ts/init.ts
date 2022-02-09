294c4bd80a177dfdc05a9ddc8ea615c7b2843561
Andrew P <professor_smart@hotmail.com>
2022-02-09 08:02:12 +0000

    Neater variable initialisation

--- site/ts/init.ts
+++ site/ts/init.ts
@@ -42,10 +42,10 @@ window.addEventListener("error", async e => {
     }
 });
 
-let dark: boolean = localStorage.getItem("Dark") == "true";
+let dark = localStorage.getItem("Dark") == "true";
 document.documentElement.classList.toggle("dark", dark);
 
-let hue: string = localStorage.getItem("Hue") || "200";
+let hue = localStorage.getItem("Hue") || "200";
 document.documentElement.style.setProperty("--main-hue", hue);
 document.documentElement.style.setProperty("--hue-rotate", `${parseFloat(hue) - 200}deg`);
 
