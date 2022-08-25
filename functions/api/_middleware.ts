5af2b2a25013929979e7cb14df8f22dd2c095c36
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2022-08-25 16:18:32 +1000

    Hopefully got honeycomb working

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -1,3 +1,7 @@
 import honeycombPlugin from "@cloudflare/pages-plugin-honeycomb";
 
-export const onRequest = honeycombPlugin();
+export const onRequest = honeycombPlugin({
+    serviceName: "paragon",
+    dataset: "test-paragon",
+    apiKey: ""
+});
