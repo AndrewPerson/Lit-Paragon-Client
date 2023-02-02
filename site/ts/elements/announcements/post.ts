be01f27f05ca7f318d08936b30ea6ef6f731ae2c
Andrew Pye <professor_smart@hotmail.com>
2023-02-03 08:29:05 +1100

    Made marking posts as read simpler

--- site/ts/elements/announcements/post.ts
+++ site/ts/elements/announcements/post.ts
@@ -39,36 +39,16 @@ export class AnnouncementPost extends LitElement {
     @property()
     content: string;
 
-    @property({ type: Number })
-    weight: number;
-
     @property()
     key: string;
 
     @query("#read")
     readIndicator: HTMLInputElement;
 
-    MarkRead(e: Event) {
-        if ((e.target as HTMLDetailsElement).open) {
-            this.readIndicator.checked = true;
-            this.SaveRead(true);
-        }
-    }
-
     SaveRead(read: boolean) {
-        let announcements = new Set<string>(JSON.parse(localStorage.getItem("Read Announcements") ?? "[]"));
+        this.readIndicator.checked = read;
 
-        if (this.parentElement !== null) {
-            for (let announcement of announcements) {
-                //This type comparison is because an older version of storing whether the announcement was read or not used to be used.
-                //This stops the code breaking if it encounters that.
-                if (typeof announcement === "string") {
-                    if (this.parentElement.querySelector(`announcement-post[key="${EscapeCss(announcement)}"]`) === null)
-                        announcements.delete(announcement);
-                }
-                else announcements.delete(announcement);
-            }
-        }
+        let announcements = new Set<string>(JSON.parse(localStorage.getItem("Read Announcements") ?? "[]"));
 
         if (read) announcements.add(this.key);
         else announcements.delete(this.key);
@@ -77,7 +57,7 @@ export class AnnouncementPost extends LitElement {
     }
 
     GetRead(): boolean {
-        let announcements: Set<string> = new Set<string>(JSON.parse(localStorage.getItem("Read Announcements") ?? "[]"));
+        let announcements = new Set<string>(JSON.parse(localStorage.getItem("Read Announcements") ?? "[]"));
 
         return announcements.has(this.key);
     }
@@ -85,7 +65,7 @@ export class AnnouncementPost extends LitElement {
     render() {
         return html`
         <input type="checkbox" id="read" ?checked="${this.GetRead()}" @input="${(e: InputEvent) => this.SaveRead((e.target as HTMLInputElement).checked)}">
-        <details @toggle="${this.MarkRead.bind(this)}">
+        <details @toggle="${(e: Event) => { if ((e.target as HTMLDetailsElement).open) this.SaveRead(true) }}">
             <summary>
                 <h1>${this.title}</h1>
                 <p class="subtitle">By ${this.author} | For ${this.years}${this.meeting ? html` | <span class="meeting">Meeting at ${this.meetingTime}, ${this.meetingDate}</span>` : ""}${this.published === null ? "" : ` | Published ${this.published}`}</p>
