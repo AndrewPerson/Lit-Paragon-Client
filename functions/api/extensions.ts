eb43c448fcc0a17d45652ed44d2480f4c1d1a6e0
Andrew Pye <professor_smart@hotmail.com>
2023-02-17 07:58:26 +1100

    Fixed invalid page calculations

--- functions/api/extensions.ts
+++ functions/api/extensions.ts
@@ -18,7 +18,7 @@ export const onRequestGet = create<SBHSEnv>("extensions", true, async ({ env, re
 
     //env comes from a trusted source so it should be okay to inject its values directly. (If someone manages to compromise the env, they could do whatever they want already.)
     const result: ExecutedQuery = await connection.execute(`SELECT * FROM \`${env.PLANETSCALE_EXTENSIONS_TABLE}\` ORDER BY \`name\` LIMIT ?,?`, [
-        pageSize * page,
+        pageSize * (page - 1),
         pageSize
     ]);
 
