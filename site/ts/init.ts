5f5b61e6defbc92641250106cfc03318fe111d13
Andrew Pye <professor_smart@hotmail.com>
2023-03-02 16:35:33 +1100

    Removed required features checking

--- site/ts/init.ts
+++ site/ts/init.ts
@@ -42,14 +42,3 @@ document.documentElement.classList.toggle("dark", dark);
 let hue = localStorage.getItem("Hue") || "200";
 document.documentElement.style.setProperty("--main-hue", hue);
 document.documentElement.style.setProperty("--hue-rotate", `${parseFloat(hue) - 200}deg`);
-
-for (let feature of REQUIRED_FEATURES) {
-    let obj: any = window;
-    for (let part of feature.split(".")) {
-        obj = obj[part];
-
-        if (obj == null || obj == undefined) {
-            location.href = `${location.origin}/unsupported?feature="${part}"`
-        }
-    }
-}
