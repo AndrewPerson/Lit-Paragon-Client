74f21a8ec0c656c4587762458b82699b72e61544
Andrew Pye <professor_smart@hotmail.com>
2023-03-03 08:54:50 +1100

    Removed ts-ignore

--- site/ts/utils.ts
+++ site/ts/utils.ts
@@ -2,7 +2,6 @@ export function Debounce(this: unknown, func: (...args: any[]) => void, timeout:
     let timer: number;
     return (...args: any[]) => {
         clearTimeout(timer);
-        // @ts-ignore
         timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
 }
