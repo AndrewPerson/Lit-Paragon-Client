99ed5cf3d785711dcba46794176db6e593c225e0
Andrew Pye <professor_smart@hotmail.com>
2023-03-02 16:33:47 +1100

    Refactored telemetry permission handling

--- site/ts/init.ts
+++ site/ts/init.ts
@@ -4,38 +4,37 @@ declare const REQUIRED_FEATURES: string[];
 declare const TELEMETRY_PERMISSION_STORAGE: string;
 //declare const TELEMETRY_STORAGE: string;
 
-window.addEventListener("error", async e => {
-    const telemetryPermission = JSON.parse(localStorage.getItem(TELEMETRY_PERMISSION_STORAGE) ?? "true");
-
-    if (!telemetryPermission) return;
-
-    if (e.error instanceof Error) {
-        fetch(`${SERVER_ENDPOINT}/error`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                error_message: e.error.message,
-                stack_trace: e.error.stack ?? "No stack trace",
-                version: VERSION
-            })
-        });
-    }
-    else {
-        fetch(`${SERVER_ENDPOINT}/error`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                error_message: "No message",
-                stack_trace: e.error.stack ?? "No stack trace",
-                version: VERSION
-            })
-        });
-    }
-})
+if (JSON.parse(localStorage.getItem(TELEMETRY_PERMISSION_STORAGE) ?? "true"))
+{
+    window.addEventListener("error", async e => {
+        if (e.error instanceof Error) {
+            fetch(`${SERVER_ENDPOINT}/error`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    error_message: e.error.message,
+                    stack_trace: e.error.stack ?? "No stack trace",
+                    version: VERSION
+                })
+            });
+        }
+        else {
+            fetch(`${SERVER_ENDPOINT}/error`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    error_message: "No message",
+                    stack_trace: e.error.stack ?? "No stack trace",
+                    version: VERSION
+                })
+            });
+        }
+    });
+}
 
 let dark = localStorage.getItem("Dark") == "true";
 document.documentElement.classList.toggle("dark", dark);
