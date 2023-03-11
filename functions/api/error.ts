051a6658acc2dd75ec2f84e7d73b13b0a21f925a
Andrew Pye <professor_smart@hotmail.com>
2023-03-12 09:55:43 +1100

    Migrated errors to use D1 database

--- functions/api/error.ts
+++ functions/api/error.ts
@@ -1,9 +1,7 @@
-import { connect } from "@planetscale/database";
-
 import { create } from "../lib/function";
 import { SBHSEnv } from "../lib/env";
 
-export const onRequestPost = create<SBHSEnv>("error", true, async ({ env, request, data: { tracer } }) => {
+export const onRequestPost = create<SBHSEnv>("error", true, async ({ env: { PARAGON_DB }, request, data: { tracer } }) => {
     const error = await request.json();
 
     if (typeof error !== "object") {
@@ -21,23 +19,17 @@ export const onRequestPost = create<SBHSEnv>("error", true, async ({ env, reques
     if (!("version" in error) || typeof error.version !== "string") {
         return new Response("Body must contain a version string", { status: 400 });
     }
-    
-    const connection = connect({
-        host: env.PLANETSCALE_HOST,
-        username: env.PLANETSCALE_USERNAME,
-        password: env.PLANETSCALE_PASSWORD,
-        fetch: tracer.fetch.bind(tracer)
-    });
 
-    //env comes from a trusted source so it should be okay to inject its values directly. (If someone manages to compromise the env, they could do whatever they want already.)
-    const result = await connection.execute(`INSERT INTO \`${env.PLANETSCALE_ERRORS_TABLE}\` (error_message, stack_trace, version, date) VALUES (?, ?, ?, ?)`, [
+    const errorRequest = PARAGON_DB.prepare("INSERT INTO errors (error_message, stack_trace, version, date) VALUES (?, ?, ?, ?)").bind(
         error.error_message,
         error.stack_trace,
         error.version,
         new Date()
-    ]);
+    );
 
-    if (result.rowsAffected != 1) {
+    const result = await errorRequest.run();
+
+    if (result.changes != 1) {
         return new Response("Failed to report error.", { status: 500 });
     }
 
