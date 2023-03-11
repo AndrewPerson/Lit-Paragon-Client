67afe575c6d4eab02dfd9491eb76b896d6ee3c54
Andrew Pye <professor_smart@hotmail.com>
2023-03-12 09:53:10 +1100

    Migrated extensions t D1 database

--- functions/lib/env.ts
+++ functions/lib/env.ts
@@ -2,9 +2,5 @@ export type SBHSEnv = {
     CLIENT_ID: string,
     CLIENT_SECRET: string,
     REDIRECT_URI: string,
-    PLANETSCALE_HOST: string,
-    PLANETSCALE_USERNAME: string,
-    PLANETSCALE_PASSWORD: string,
-    PLANETSCALE_ERRORS_TABLE: string,
-    PLANETSCALE_EXTENSIONS_TABLE: string
+    PARAGON_DB: D1Database
 }
